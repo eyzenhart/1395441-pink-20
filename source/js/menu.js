06693719d37a4523656f73e0f6ac975ae3761797
eyzenhart <eyzenhart@gmail.com>
2020-08-30 13:09:13 +0300

    Улучшает сетки главной страницы и добавляет сетки странице с фотографиями

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,24 +1,19 @@
 var menuLink = document.querySelector(".main-nav__toggle");
 var menuPopup = document.querySelector(".main-nav__popup");
-var menuOpen = document.querySelector(".main-nav__toggle_open");
-var menuClose = document.querySelector(".main-nav__toggle_close");
 var menuWrapper = document.querySelector(".page-header__wrapper");
-
 
 menuLink.addEventListener("click", function (evt) {
   evt.preventDefault();
-  menuPopup.classList.remove("main-nav_hidden");
-  menuPopup.classList.add("modal-show");
-  menuLink.classList.remove("main-nav__toggle_open");
-  menuLink.classList.add("main-nav__toggle_close");
-  menuWrapper.classList.add("page-header__wrapper_dark");
-});
-
-menuClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  menuPopup.classList.remove("modal-show");
-  menuPopup.classList.add("main-nav_hidden");
-  menuLink.classList.remove("main-nav__toggle_close");
-  menuLink.classList.add("main-nav__toggle_open");
-  menuWrapper.classList.remove("page-header__wrapper_dark");
+  if (menuLink.classList.contains("main-nav__toggle_open")) {
+    menuLink.classList.remove("main-nav__toggle_open");
+    menuLink.classList.add("main-nav__toggle_close");
+    menuPopup.classList.add("modal-show");
+    menuWrapper.classList.add("page-header__wrapper_dark");
+  } else {
+    menuLink.classList.remove("main-nav__toggle_close");
+    menuLink.classList.add("main-nav__toggle_open");
+    menuWrapper.classList.remove("page-header__wrapper_dark");
+    menuPopup.classList.remove("modal-show");
+    menuPopup.classList.add("main-nav_hidden");
+  }
 });
